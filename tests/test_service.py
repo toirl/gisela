85741e59bebf6c3062958584ebaf340f4a9cdba3
Torsten Irländer <torsten.irlaender@googlemail.com>
2015-12-21 21:40:46 +0100

    Make JSON request on POST, PUT and DELETE

--- tests/test_service.py
+++ tests/test_service.py
@@ -72,7 +72,7 @@ class TestTagService(unittest.TestCase):
         assert response.status == '200 OK'
 
     def test_create(self):
-        response = self.app.post("/tags",
+        response = self.app.post_json("/tags",
                                  {"name": "New",
                                   "description": "New description"})
         assert response.status == '201 OK'
@@ -82,14 +82,14 @@ class TestTagService(unittest.TestCase):
         assert response.status == '200 OK'
 
     def test_update(self):
-        response = self.app.put("/tags/1",
-                                {"name": "Foo2",
-                                 "description": "Changed description"})
+        response = self.app.put_json("/tags/1",
+                                    {"name": "Foo2",
+                                     "description": "Changed description"})
         assert response.status == '200 OK'
         assert response.json["data"]["name"] == "Foo2"
 
     def test_delete(self):
-        response = self.app.delete("/tags/3")
+        response = self.app.delete_json("/tags/3")
         assert response.status == '204 OK'
 
 
@@ -110,10 +110,13 @@ class TestTimeService(unittest.TestCase):
         assert response.status == '200 OK'
 
     def test_create(self):
-        response = self.app.post("/times",
-                                 {"start_date": "2015-01-01",
-                                  "duration": 25,
-                                  "description": "25 minutes"})
+        from datetime import datetime
+        response = self.app.post_json("/times",
+                                      {"name": "Bar",
+                                       "start_date": "2015-11-18",
+                                       "duration": 50,
+                                       "description": "Bar description",
+                                       "tags": [1, 2]})
         assert response.status == '201 OK'
 
     def test_read(self):
@@ -121,36 +124,39 @@ class TestTimeService(unittest.TestCase):
         assert response.status == '200 OK'
 
     def test_update(self):
-        response = self.app.put("/times/1",
-                                {"start_date": "2015-11-01",
-                                 "duration": 50,
-                                 "description": "Changed"})
+        response = self.app.put_json("/times/1",
+                                     {"name": "Bar",
+                                      "start_date": "2015-11-01",
+                                      "duration": 50,
+                                      "description": "Changed",
+                                      "tags": [1]})
         assert response.status == '200 OK'
         assert response.json["data"]["start_date"].find("2015-11-01") > -1
         assert response.json["data"]["duration"] == 50
         assert response.json["data"]["description"] == "Changed"
+        assert len(response.json["data"]["tags"]) == 1
 
     def test_delete(self):
-        response = self.app.delete("/times/3")
+        response = self.app.delete_json("/times/3")
         assert response.status == '204 OK'
 
     def test_start(self):
-        response = self.app.put("/times/1/start")
+        response = self.app.put_json("/times/1/start")
         assert response.json["data"]["state"] == 1
         assert response.status == '200 OK'
         response = self.app.put("/times/1/stop")
 
     def test_pause(self):
-        response = self.app.put("/times/1/start")
-        response = self.app.put("/times/1/pause")
+        response = self.app.put_json("/times/1/start")
+        response = self.app.put_json("/times/1/pause")
         assert response.json["data"]["state"] == 2
         assert response.status == '200 OK'
-        response = self.app.put("/times/1/stop")
+        response = self.app.put_json("/times/1/stop")
         assert response.status == '200 OK'
 
     def test_stop(self):
-        response = self.app.put("/times/1/start")
-        response = self.app.put("/times/1/stop")
+        response = self.app.put_json("/times/1/start")
+        response = self.app.put_json("/times/1/stop")
         assert response.json["data"]["state"] == 0
         assert response.status == '200 OK'
 
